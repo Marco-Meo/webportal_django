783830ae5f7d76ae0c295f51cb6a7461b97584b8
marco-meo <marco@meonet.ch>
2020-12-10 14:09:33 +0100

    FAQ Create hinzugefügt

--- faq/views.py
+++ faq/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Faq
 from .forms import FaqForm
+from django.contrib.auth.decorators import login_required
+
 
 def faq_list(request):
     faqs = Faq.objects.all()
@@ -24,7 +26,14 @@ def faq_update(request, pk):
     return render(request, 'faq/faq_update.html', {'form': form})
 
 
+@login_required(login_url='/admin/login/')
 def faq_create(request):
     form = FaqForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            faq_form = form.save(commit=False)
+            faq_form.creator = request.user
+            faq_form.save()
+            return redirect('faq:faq-list')
 
     return render(request, 'faq/faq_create.html', {'form': form})
