01841169e85d482e466afd612567c60ea5d8dda5
marco-meo <marco@meonet.ch>
2020-12-10 16:44:31 +0100

    Model mit Hilfetext erweitert.

--- faq/models.py
+++ faq/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from datetime import datetime
 
 class Faq(models.Model):
-    title = models.CharField('Titel', max_length=200)
-    description = models.CharField('Beschreibung', max_length=1000)
+    title = models.CharField('Titel', max_length=200, help_text="Titel des FAQ Eintrages")
+    description = models.CharField('Beschreibung', max_length=1000, help_text="Hilfestellung beschreiben")
     creation_date = models.DateTimeField(auto_now=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
